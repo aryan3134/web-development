78b1bce9a110ea6c76a9a5a6fca091b9c43d3796
aryan3134 <aryan3134.beai24@chitkara.edu.in>
2025-01-28 12:21:44 +0530

    session added to our web application

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
-# creating a instance of class flask
-app = Flask(_name_)
 
-# "/" stands for home direcotry
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "zyx"
+
+app.permanent_session_lifetime = timedelta(days=2)
+
 @app.route("/")
 def home():
-    #rednering index.html file
     return render_template("index.html")
 
-#login page methods allowed psot and get
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    # request lib handles all the request of our web page
-    # if mehtod of event is post send user to his local page
     if request.method == "POST":
-        # form is a dicnaroy and we are retriveing user name from key 'nm' decleared in html form tag
+        session.permanent = True
         user = request.form["nm"]
-        #after submit we are redirectoring user to his page and passing user as attribute
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
-        # if no submit evnet is generated send user to login page and ask user to login
-        return render_template("login.html")
+        if "user" in session:
+            return redirect(url_for("user"))
+        else:
+            return render_template("login.html")
 
-# <usr> is used to send variable to our method
-@app.route("/<usr>")
-def user(usr):
-    # we are rending a inline html to show user name
-    return f"<h1>{usr}</h1>"
 
-#entry point of our web page
-if _name_ == "_main_":
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}<h1/>"
+    else:
+        return redirect(url_for("login"))
+
+if __name__ == "__main__":
     app.run()
